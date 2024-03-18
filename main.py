00cb3bb546d68860bc3b01b5a9c0c40bcfcd3c8a
liangjiachen <1074847834@qq.com>
2024-03-18 10:49:14 +0800

    algorithm implementation

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
 import mmh3
-dataset=[]
-attacker=[]
-seed1=1000
-seed2=3341
 class Detector:
-    def __init__(self,m,p):
-        self.bit_array_src=[0]*m
-        self.bit_array_dst=[0]*m
+    def __init__(self,m,p,t):
+        self.bit_array=[0]*m
         self.samping_probability=p
-        self.cnt1=0
-        self.cnt2=0
+        self.offline_hashmap_src={}
+        self.offline_hashmap_dst={}
+        self.detect_threshold=t
+        self.cnt=0
+        self.seed1=1000
+        self.m=m
+        self.seed2=3341
     def insert(self,src,dst):
-        hash_result=0
+        sampling_probability=self.samping_probability*self.m/(self.m-self.cnt)
+        hash_result_1=mmh3.hash64(src^dst,self.seed1)
+        hash_result_2=mmh3.hash64(src^dst,self.seed2)%self.m
+        is_sampled=hash_result_1<sampling_probability*2**64
+        if self.bit_array[hash_result_2]==0:
+            self.cnt+=1
+            self.bit_array[hash_result_2]=1
+            if is_sampled:
+                if src in self.offline_hashmap_src:
+                    self.offline_hashmap_src[src]+=1
+                else:
+                    self.offline_hashmap_src[src]=1
+                if dst in self.offline_hashmap_dst:
+                    self.offline_hashmap_dst[dst]+=1
+                else:
+                    self.offline_hashmap_dst[dst]=1
+    def detect(self,src,dst):
+        if src not in self.offline_hashmap_src or dst not in self.offline_hashmap_dst:
+            return False
+        src_flow_spread_estimation=self.offline_hashmap_src[src]/self.samping_probability
+        dst_flow_spread_estimation=self.offline_hashmap_dst[dst]/self.samping_probability
+        return dst_flow_spread_estimation-src_flow_spread_estimation>self.detect_threshold*dst_flow_spread_estimation
+
+if __name__=="main":
+    dataset=[]
+    ddos_dataset=[]
+    detected_victim=[]
+    actual_victim=[]
+    
+
